c191f53e09cc5a23b15da0939f5996d77bd963d2
Ubendran R G <Ubendran R G@Ubendran>
2022-04-12 11:39:45 +0530

    commit 1

---
+++ com.crm.comcast.GitPractice/src/test/java/com/crm/comcast/GitPractice/ClassA.java
@@ -0,0 +1,11 @@
+package com.crm.comcast.GitPractice;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+
+	@Test
+	public void firstPush() {
+		System.out.println("first push");
+	}
+}
